5bd45e6d129ac38115f8edc5e51c2b6b8fdaa229
Andrew Vargas <andrewvargas6933@gmail.com>
2018-04-04 22:59:38 -0500

    Created Files

---
+++ blog/config.py
@@ -0,0 +1,4 @@
+import os
+class DevelopmentConfig(object):
+    SQLALQEMY_DATABASE_URI = "postgresl://avargas:Thinkful@localhost:5432/blogful"
+    DEBUG = True
---
+++ manage.py
@@ -0,0 +1,16 @@
+import os
+from flask_script import Manager
+
+from blog import app
+
+manager = Manager(app)
+
+
+@manager.command
+def run():
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
+
+if __name__ == "__main":
+    manager.run()
